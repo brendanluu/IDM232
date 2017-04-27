c9ca77aa90d0b7d9ba15e012b4f0fd71fd243b26
brendanluu <bkl34@drexel.edu>
2017-04-27 19:56:30 -0400

    my first experiment with php

---
+++ week04/pageDuce.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page2</title>
+  </head>
+  <body>
+    <pre>
+      <?php
+          print_r($_GET);
+       ?>
+    </pre>
+
+  </body>
+</html>
